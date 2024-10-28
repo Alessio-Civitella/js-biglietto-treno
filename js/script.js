76dcf1345e56e9c8df52b03bdd7321cde20818dd
Alessio-Civitella <alessio.civitella19@gmail.com>
2024-10-28 15:40:31 +0100

    Feat: variabili impostate

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Variabili per il numero di chilometri e l'età
+const km = parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere:"));
+const eta = parseInt(prompt("Inserisci l'età del passeggero:"));
+
