406c32bdb54233fa7647dedc3a6946336326096f
Alessio-Civitella <alessio.civitella19@gmail.com>
2024-10-28 15:42:25 +0100

    Feat: calcolo prezzo senza sconti

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@
 const km = parseFloat(prompt("Inserisci il numero di chilometri che vuoi percorrere:"));
 const eta = parseInt(prompt("Inserisci l'età del passeggero:"));
 
+if (!isNaN(km) && !isNaN(eta) && km >= 0 && eta >= 0) {
+    const prezzoPerKm = 0.21; // Prezzo per km
+    let prezzoTotale = km * prezzoPerKm; // Calcolo prezzo totale senza sconto
+
