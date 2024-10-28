84b523f0b392b3317a135534a6789b446e0c5d17
Alessio-Civitella <alessio.civitella19@gmail.com>
2024-10-28 15:44:23 +0100

    Feat : applicazione sconti

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ if (!isNaN(km) && !isNaN(eta) && km >= 0 && eta >= 0) {
     const prezzoPerKm = 0.21; // Prezzo per km
     let prezzoTotale = km * prezzoPerKm; // Calcolo prezzo totale senza sconto
 
+    // Applicazione degli sconti
+    if (eta < 18) {
+        prezzoTotale *= 0.8; // Sconto del 20% per i minorenni
+    } else if (eta > 65) {
+        prezzoTotale *= 0.6; // Sconto del 40% per gli over 65
+    }
+
+
+
